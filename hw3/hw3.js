fd634eee8616f37da1fb2ee089936e8f911cbd48
Nick <nikmaunt@gmail.com>
2023-08-11 18:30:31 +0400

    feat: hw3

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -1,11 +1,32 @@
-
-function myFilter () {
-	let filterNum= [];
-	for(let i=0; i<numbers.length; i++){
-		if(numbers[i]>5) filterNum.push( numbers[i] );
+Array.prototype.myFilter = function (callback, thisArg) {
+	if (typeof callback !== 'function') {
+		throw new TypeError('Callback must be a function');
 	}
+
+	const newArray = [];
+	const array = this;
+
+	for (let i = 0; i < array.length; i++) {
+		if (i in array) {
+			const currentItem = array[i];
+			if (callback.call(thisArg, currentItem, i, array)) {
+				newArray.push(currentItem);
+			}
+		}
+	}
+
+	return newArray;
+};
+
+function createDebounceFunction(callback, delay) {
+	let timerId;
+
+	return function () {
+		clearTimeout(timerId);
+
+		timerId = setTimeout(() => {
+			callback.apply(this, arguments);
+		}, delay);
+	};
 }
 
-
-document.write(`Исходный массив: ${numbers}<br>
-  Отфильтрованный массив: ${filterNum}`);
