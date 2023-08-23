2db9c8dc79d07f96a2f053f27944726a39563865
Nick <nikmaunt@gmail.com>
2023-08-23 14:25:42 +0400

    feat: hw5

--- hw5/stack.js
+++ hw5/stack.js
@@ -85,7 +85,7 @@ class LinkedList {
 	}
 
 	append(elem) {
-		const newNode = { value: elem, next: null };
+		const newNode = new Node(elem, this.head);
 
 		if (!this.head) {
 			this.head = newNode;
@@ -106,6 +106,41 @@ class LinkedList {
 			this.head = newNode;
 		}
 	}
+
+	static fromIterable(iterable) {
+		if (!Symbol.iterator in Object(iterable)) {
+			throw new Error('Iterable is required');
+		}
+
+		const linkedList = new LinkedList();
+
+		for (const item of iterable) {
+			linkedList.append(item);
+		}
+
+		return linkedList;
+	}
 }
 
-module.exports = { Stack, LinkedList };
+// module.exports = { Stack, LinkedList };
+const linkedList = new LinkedList();
+
+// Тест метода append
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+console.log(linkedList); // Output: { head: { value: 1, next: { value: 2, next: [Object] } }, tail: { value: 3, next: null } }
+
+// Тест метода prepend
+linkedList.prepend(0);
+
+console.log(linkedList); // Output: { head: { value: 0, next: { value: 1, next: [Object] } }, tail: { value: 3, next: null } }
+
+console.log(linkedList); // Output: { head: { value: 0, next: { value: 1, next: [Object] } }, tail: { value: 3, next: null } }
+
+// Тест метода fromIterable
+const iterable = [4, 5, 6];
+const newList = LinkedList.fromIterable(iterable);
+
+console.log(newList); // Output: { head: { value: 4, next: { value: 5, next: [Object] } }, tail: { value: 6, next: null } }
